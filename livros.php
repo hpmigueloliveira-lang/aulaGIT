42599103307f1598b16c4a087b49fa422f01ec3f
Matheus Pieretti <35925@fai.com.br>
2026-09-18 16:22:52 -0300

    correção

--- livros.php
+++ livros.php
@@ -1,16 +1,14 @@
 <?php
 // livros.php - Gestão de Livros da Livraria
 
-// Exemplo de lista de livros (simulando um banco de dados)
 $livros = [
-    ["id" => 1, "titulo" => "O Alquimista", "autor" => "Paulo Coelho", "preco" => 29.90],
-    ["id" => 2, "titulo" => "Dom Casmurro", "autor" => "Machado de Assis", "preco" => 19.90]
+    ["id" => 1, "titulo" => "O Alquimista", "autor" => "Paulo Coelho", "preco" => 29.90, "quantidade" => 10],
+    ["id" => 2, "titulo" => "Dom Casmurro", "autor" => "Machado de Assis", "preco" => 19.90, "quantidade" => 5]
 ];
 
-// Função simples para listar os livros
 function listarLivros($lista) {
     foreach ($lista as $livro) {
-        echo "ID: " . $livro['id'] . " | Título: " . $livro['titulo'] . " | Autor: " . $livro['autor'] . " | Preço: R$ " . $livro['preco'] . "\n";
+        echo "ID: " . $livro['id'] . " | Título: " . $livro['titulo'] . " | Autor: " . $livro['autor'] . " | Preço: R$ " . $livro['preco'] . " | Qtd: " . $livro['quantidade'] . "\n";
     }
 }
 
